dfe9a171ea1be012f417bcdff1456bf3389db3c1
201411096 <korea5781@gmail.com>
2021-12-02 14:46:09 +0900

    dfs 재귀

--- 프로그래머스/코딩테스트 고득점 Kit/8_DFS&BFS/연습/dfs.py
+++ 프로그래머스/코딩테스트 고득점 Kit/8_DFS&BFS/연습/dfs.py
@@ -14,6 +14,18 @@ graph = {
     'M': ['H']
 }
 
+graph2 = {
+    'A' : ['B', 'C'],
+    'B' : ['A', 'D'],
+    'C' : ['A', 'G', 'H', 'I'],
+    'D' : ['B', 'E', 'F'],
+    'E' : ['D'],
+    'F' : ['D'],
+    'G' : ['C'],
+    'H' : ['C'],
+    'I' : ['C', 'J'],
+    'J' : ['I']
+}
 
 def dfs(graph, start_node):
     visited = list()
@@ -29,4 +41,16 @@ def dfs(graph, start_node):
     
     return visited
 
-print(dfs(graph, 'A'))  # ['A', 'B', 'H', 'M', 'J', 'K', 'L', 'I', 'C', 'D', 'G', 'E', 'F']
+def dfs_recursive(graph, start_node, visited=[]):
+    visited.append(start_node)
+    
+    for node in graph[start_node]:
+        if node not in visited:
+            dfs_recursive(graph, node, visited)
+    
+    return visited
+
+print(dfs(graph, 'A'))                      # ['A', 'B', 'H', 'M', 'J', 'K', 'L', 'I', 'C', 'D', 'G', 'E', 'F']
+print(dfs(graph2, 'A'))                     # ['A', 'C', 'I', 'J', 'H', 'G', 'B', 'D', 'F', 'E']
+print(dfs_recursive(graph, 'A', []))        # ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+print(dfs_recursive(graph2, 'A', []))       # ['A', 'B', 'D', 'E', 'F', 'C', 'G', 'H', 'I', 'J']
