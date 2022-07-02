febeeb2d06d4be29e02ec19da3d30c2242379ae7
201411096 <korea5781@gmail.com>
2022-07-02 15:34:43 +0900

    가장 큰 수

---
+++ 프로그래머스/코딩테스트 고득점 Kit/4_정렬/maxNumber.py
@@ -0,0 +1,43 @@
+"""
+프로그래머스 -> 코딩테스트 연습 -> 정렬 -> 가장 큰 수
+
+
+문제 설명
+0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+
+예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+
+0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+numbers의 길이는 1 이상 100,000 이하입니다.
+numbers의 원소는 0 이상 1,000 이하입니다.
+정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+
+입출력 예
+numbers	return
+[6, 10, 2]	"6210"
+[3, 30, 34, 5, 9]	"9534330"
+
+"""
+import functools
+
+def solution(numbers):
+    def myCmp(n1, n2):
+        return -(int(n1+n2) - int(n2+n1))
+    
+    for i, number in enumerate(numbers):
+        numbers[i] = str(number)
+
+    answer = "".join([str(i) for i in sorted(numbers, key=functools.cmp_to_key(myCmp))])
+
+    if(answer[0]=='0'):
+        answer = '0'
+    return answer
+
+
+
+numbers = [6, 10, 2]
+print(solution(numbers)) # "6210"
+numbers = [3, 30, 34, 5, 9]
+print(solution(numbers)) # "9534330"
