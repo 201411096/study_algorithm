2576f871cf5f7a47350c835998b424d1c4f972a6
201411096 <korea5781@gmail.com>
2022-07-11 15:04:37 +0900

    unionFind.py

---
+++ programmers/codingTest highScore  kit/6_greedy/practice/unionFind.py
@@ -0,0 +1,57 @@
+# union-find    : 합집합 찾기
+# disjoint-set  : 서로소 집합
+
+def makeSet(n):
+    return [i for i in range(n+1)]
+
+def findRoot(parents, n):
+    if parents[n] == n:
+        return n
+    else:
+        return findRoot(parents, parents[n])
+
+def unionRoot(parents, a, b):
+    rootA = findRoot(parents, a)
+    rootB = findRoot(parents, b)
+
+    if rootA < rootB:
+        parents[rootB] = rootA
+    else:
+        parents[rootA] = rootB
+
+def compareRoot(parents, a, b):
+    rootA = findRoot(parents, a)
+    rootB = findRoot(parents, b)
+
+    if rootA == rootB:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    n = 8
+
+    parents = makeSet(n)
+
+    unionRoot(parents, 1, 2) # [ [1, 2] ]
+    unionRoot(parents, 2, 3) # [ [1, 2, 3] ]
+    unionRoot(parents, 4, 3) # [ [1, 2, 3, 4] ]
+    unionRoot(parents, 5, 6) # [ [1, 2, 3, 4], [5, 6] ]
+    unionRoot(parents, 7, 8) # [ [1, 2, 3, 4], [5, 6], [7, 8] ]
+
+    print('='*25)
+    print('4,1 => same set? :', compareRoot(parents, 4, 1))    # True
+    print('4,5 => same set? :', compareRoot(parents, 4, 5))    # False
+    print('5,6 => same set? :', compareRoot(parents, 5, 6))    # True
+    print('5,7 => same set? :', compareRoot(parents, 5, 7))    # False
+    print('4,7 => same set? :', compareRoot(parents, 4, 7))    # False
+    print('7,8 => same set? :', compareRoot(parents, 7, 8))    # True
+
+    unionRoot(parents, 5, 4)
+    unionRoot(parents, 6, 7)
+    print('='*25)
+
+    print('4,5 => same set? :', compareRoot(parents, 4, 5))    # True
+    print('5,7 => same set? :', compareRoot(parents, 5, 7))    # True
+    print('4,7 => same set? :', compareRoot(parents, 4, 7))    # True
+    print('='*25)
